735570212d4efc958ac1432a592ff5b38404c159
Kelvin Scherer <kelvinscherer@gmail.com>
2016-06-17 19:23:27 -0300

    Adiciona função de venda
    Alterações no código.

--- ExperimentalProject/core/admin.py
+++ ExperimentalProject/core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
 from ExperimentalProject.core.forms import PersonForm, ProductForm, PaymentForm
-from ExperimentalProject.core.models import Person, Product, Payment
+from ExperimentalProject.core.models import Person, Product, Payment, Sale, SaleItem
 
 
 class PersonAdmin(admin.ModelAdmin):
@@ -21,10 +20,27 @@ class ProductAdmin(admin.ModelAdmin):
 class PaymentAdmin(admin.ModelAdmin):
     form = PaymentForm
     list_display = ('name', 'addtions')
-    search_fields = ('name')
+    search_fields = ('name',)
 
+
+class SaleItemAdmin(admin.TabularInline):
+    model = SaleItem
+    extra = 1
+    
+    
+class SalaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (Nome, {'fields': ['client']}),
+        (Nome, {'fields': ['payment']}),
+        ]
+    inlines = [SaleItemAdmin]
+    model = Sale
+        
+    list_display = ('person', 'get_total', 'payment', 'created_at')
+        
 
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Sale, SaleAdmin)
 admin.site.site_header = 'Super Simple Sale (3S)'
